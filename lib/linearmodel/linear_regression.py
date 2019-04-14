Fixed error in Batch Gradient Descent

lib/linearmodel/linear_regression.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 13 19:27:26 2018
+Created on Sun Apr 14 12:26:30 2019
 
 @author: pmaddala
 """
@@ -14,27 +14,35 @@ class Cost(Enum):
     MSE = 1
     MAE = 2
     
+class Optimizer(Enum):
+    SGD = 1
+    BATCH_GRAD = 2
+    MINI_BATCH_GRAD = 3
+    
 class LinearRegression:
-    def __init__(self,x,y,alpha=0.01,cost=Cost.MSE,L=0):
+    def __init__(self,x,y,alpha=0.01,cost=Cost.MSE,Lambda=0,optimizer=Optimizer.BATCH_GRAD):
         self.input = x
         t = []
+        #Adding dimension X[0]=1, to accomodate bias
         for i in range(len(x)):
             t.append(np.insert(x[i],0,1))
         self.x = np.array(t)  
         self.y = y
         self.alpha = alpha
         self.coef = np.random.random(1+len(x[0]))
-        self.coef = np.zeros(1+len(x[0]))
         self.cost = cost
         self.errors = []
-        self.L = L
+        self.optimizer = optimizer
+        #Regularization parameter
+        self.Lambda = Lambda
         
-    def train(self,iterations=100):        
-        for i in range(iterations):
-            error = self.getError()
-            self.errors.append(error)
-            #print('Coef is : ',self.coef)
-            self.coef = self.coef - self.alpha*self.diff()
+    def train(self,iterations=100):
+        if self.optimizer == Optimizer.BATCH_GRAD:
+            for i in range(iterations):
+                #Find error and add it to the list, just for display purposes
+                error = self.getError(self.x,self.y)
+                self.errors.append(error)
+                self.coef = self.coef - self.alpha*self.diff(self.x,self.y)
             
     def train_tolerannce(self,e=0.05):
         error = self.getError()
@@ -49,48 +57,27 @@ class LinearRegression:
             prev = terr
             
         
-    def getError(self):
+    def getError(self,x,y):
         if self.cost == Cost.MSE:
             error = 0
             for i in range(len(x)):
-                error = error + (self.y[i]-np.dot(self.coef,self.x[i]))**2
-            return math.sqrt(error/len(self.x))
-            #return math.sqrt(np.sum((self.y-np.dot(self.coef,self.x))**2)/len(self.x))
+                error = error + (y[i]-np.dot(self.coef,x[i]))**2
+            return math.sqrt(error)/len(x)
         if self.cost == Cost.MAE:
             error = 0
             for i in range(len(x)):
                 error = error + abs(self.y[i]-np.dot(self.coef,self.x[i]))
             return error
                 
-    def diff(self):
-        if self.cost == Cost.MSE:
+    def diff(self,x,y):
+        if self.optimizer == Optimizer.BATCH_GRAD:
             tcoef = []
             for c in range(len(self.coef)):
-                error = 0
-                for i in range(len(self.x)):
-                    error = error - (self.y[i]-np.dot(self.coef,self.x[i]))*self.x[i][c]
-                error = error + self.L*self.coef[c]
-                error = 2*error
-                
-                tcoef.append(error)
+                const = 0
+                for i in range(len(x)):
+                    const = const +(-2/len(x))* (self.y[i]-np.dot(self.coef,self.x[i]))*(self.x[i][c])
+                tcoef.append(const)
             tcoef = np.array(tcoef)
-            
-            return tcoef
-        
-        if self.cost == Cost.MAE:
-            tcoef = []
-            for c in range(len(self.coef)):
-                error = 0
-                for i in range(len(self.x)):
-                    if self.y[i]-np.dot(self.coef,self.x[i])>0:
-                        error = error -self.x[i][c]
-                    else:
-                        error = error + self.x[i][c]
-                    #error = error - (self.y[i]-np.dot(self.coef,self.x[i]))*self.x[i][c]
-                #error = 2*error
-                
-                tcoef.append(error)
-            tcoef = np.array(tcoef)            
             return tcoef
             
     def predict(self,x_new):        
@@ -98,16 +85,21 @@ class LinearRegression:
         
     def plotInput(self):
         plt.scatter(self.input,self.y)
-        plt.plot(np.linspace(0,10,1000),self.coef[1]*np.linspace(0,10,1000) + self.coef[0])
+        plt.plot(np.linspace(0,10,100),self.coef[1]*np.linspace(0,10,100) + self.coef[0])
         
     def plot_errors(self):
         plt.plot(range(len(self.errors)),self.errors)
         
         
 if __name__ == '__main__':
-    x = np.array([[1],[5],[3]])
-    yl = [5,13,14]
-    y = np.array(yl)
-    l = LinearRegression(x,y,cost=Cost.MSE,L=0)
-    l.train()
+    x = []  
+    y = []
+    for i in range(10):
+        x.append([i])
+        y.append(3*i-2+np.random.randint(-2,3))
+        
+    x = np.array(x)
+    y = np.array(y)
+    l = LinearRegression(x,y,cost=Cost.MSE,Lambda=0,optimizer=Optimizer.BATCH_GRAD)
+    l.train(iterations=1000)
     l.plotInput()